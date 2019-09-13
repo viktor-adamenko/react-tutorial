48b662328f8d4b5bdf0ecf1bd82fc9cde1cbc455
Адаменко Виктор Сергеевич <viktor.adamenko@beeper.ua>
2019-09-13 11:51:21 +0300

    commit +

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from "./components/App";
+function App() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  );
+}
 
-ReactDOM.render( 
-  <App/>,  
-  document.getElementById('root')  
-); 
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
